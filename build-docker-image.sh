4690193fac5037ddae39e6ffae57fab926679646
sudheera <catchsudheera@gmail.com>
2022-01-22 23:07:22 +0100

    Released 1.0.2

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -3,4 +3,4 @@ mvn clean install && \
 docker buildx build \
 --push \
 --platform linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/amd64 \
---tag catchsudheera/emby-library-update-connector:1.0.2-SNAPSHOT .
+--tag catchsudheera/emby-library-update-connector:1.0.3-SNAPSHOT .
